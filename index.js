0798bab737dfe75dec22c97444ebdf65a7b49e72
xsin-dev <sayfullayev0510@gmail.com>
2026-01-16 16:43:39 +0500

    update

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1-topshiriq
+// const input = "abc 123 test 4567 js";
+
+// const number = "1234567890";
+
+// const output = input.split(" ").filter((word) => {
+//   for (let char of word) {
+//     if (!number.includes(char)) {
+//       return false;
+//     }
+//   }
+//   return word.length > 0;
+// });
+
+// console.log(output);
+
+// =========================================
+// 2-topshiriq
+// So‘zlarni teskari tartibda yozish
+// Input: "hello world javascript"
+
+// Output: "olleh dlrow tpircsavaj"
+
+// const input = "hello world javascript";
+// 1-usul
+// const output = input.split("").reverse().join("")
+// const result = output.split(" ").reverse().join(" ")
+// 2-usul
+// const output = input
+//   .split(" ")
+//   .map((word) => {
+//     return word.split("").reverse().join("");
+//   })
+//   .join(" ");
+// console.log(output);
+
+// =============================================
+
